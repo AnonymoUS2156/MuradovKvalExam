1dbcbf5bf50f3cc6967d1292a3efe80148dec8fe
AnonymoUS2156 <48409221+AnonymoUS2156@users.noreply.github.com>
2023-07-05 12:14:42 +0300

    Add files via upload

---
+++ ClassMuradov/ClassMuradov/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMuradov
+{
+    //Мурадов И.М.
+    public class Building
+    {
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+        public List<Apartment> Apartments = new List<Apartment>();
+
+        public Building(string address, string description)
+        {
+            Address = address;
+            Description = description;
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($"Строение Адрес {Address} Описание {Description}");
+        }
+            
+    }
+
+    public class Apartment: IComparable<Apartment>
+    {
+        public int Number { get; set; }
+
+        public string Owner { get; set; }
+
+        public Apartment(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine ($"Номер квартиры: {Number}\nВладелец: {Owner}");
+        }
+        public int CompareTo(Apartment other)
+        {
+            if (this.Number < other.Number) return -1;
+            if (this.Number > other.Number) return 1;
+            else return 0;
+        }    
+    }
+}
---
+++ ClassMuradov/Muradov/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassMuradov;
+
+namespace Muradov
+{
+    internal class Program
+    {
+        //Мурадов И.М.
+        static void Main(string[] args)
+        {
+            Building bl = new Building("г. Москва", "Многоэтажный дом");
+            bl.ShowAll();
+            Apartment a1 = new Apartment(1, "Иванов И.А.");
+            bl.Apartments.Add(a1);
+
+            Apartment a4 = new Apartment(7, "Мурадов И.М.");
+            bl.Apartments.Add(a4);
+            Apartment a2 = new Apartment(9, "Абдурахимов Б.А.");
+            bl.Apartments.Add(a2);
+            Apartment a3 = new Apartment(5, "Залевский А.А.");
+            bl.Apartments.Add(a3);
+
+            bl.Apartments.Sort();
+            foreach (Apartment a in bl.Apartments)
+            {
+                a.Show();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
